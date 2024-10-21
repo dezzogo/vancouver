72b8f33e89cca4d386c80bcea9f32991d5f55ad1
dezzogo <122938506+dezzogo@users.noreply.github.com>
2024-10-21 12:29:23 -0300

    first commit (delayed)

---
+++ everyday/ec_form.js
@@ -0,0 +1,29 @@
+//EMAIL
+var emailEC = //copiar apenas abaixo
+function () {
+    var regex = /\S+@\S+\.\S+/;
+    var email = document.querySelector('[type="email"]');
+    if(!email) return;
+    if(!regex.test(email.value)) return;
+    return email.value;
+}
+
+//PHONE
+var phoneEC = //copiar apenas abaixo
+function phone() {
+    var g_countryCode = '55';
+    var g_getPhone = document.querySelector('[type="phone"]').value;
+    g_getPhone = g_countryCode + g_getPhone;
+    g_getPhone = g_getPhone.replace(/\D/g, '');
+    if(g_getPhone.length >= 11 && g_getPhone.length <= 15) {
+      return "+" + g_getPhone
+    }
+}
+
+//PHONE QUE JÁ TEM +55
+function phone() {
+  var g_getPhone = document.querySelector('SELETOR AQUI').value.replace(/\D/g, '')
+  if(g_getPhone.length >= 11 && g_getPhone.length <= 15) {
+     return "+" + g_getPhone
+  }
+}
