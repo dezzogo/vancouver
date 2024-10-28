1187061daa28347313044e8aa5ce2dedc8b6a8e0
dezzogo <122938506+dezzogo@users.noreply.github.com>
2024-10-28 11:08:44 -0300

    phone rd local ddi

--- everyday/ec_form.js
+++ everyday/ec_form.js
@@ -10,7 +10,7 @@ function () {
 
 //PHONE
 var phoneEC = //copiar apenas abaixo
-function phone() {
+function () {
     var g_countryCode = '55';
     var g_getPhone = document.querySelector('[type="phone"]').value;
     g_getPhone = g_countryCode + g_getPhone;
@@ -20,8 +20,9 @@ function phone() {
     }
 }
 
-//PHONE QUE JÁ TEM +55
-function phone() {
+//PHONE QUE JÁ TEM +55 (RD)
+var phone55 = 
+function () {
   var g_getPhone = document.querySelector('SELETOR AQUI').value.replace(/\D/g, '')
   if(g_getPhone.length >= 11 && g_getPhone.length <= 15) {
      return "+" + g_getPhone
