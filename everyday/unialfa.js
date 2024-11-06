d5f43865c4d8be813b4ed8841a225d9f7462cb6a
dezzogo <122938506+dezzogo@users.noreply.github.com>
2024-11-06 16:29:20 -0300

    creating custom code to vestibular online, enem, transferência and diploma buttons

---
+++ everyday/unialfa.js
@@ -0,0 +1,12 @@
+// <script>
+
+var botaoVestibular = document.querySelector('[value="COMEÇAR O VESTIBULAR"]');
+
+if (botaoVestibular) {
+  dataLayer.push({ event: "vestibular_online" });
+}
+
+// https://www.unialfa.com.br/graduacao/administracao/
+
+
+// </script>
