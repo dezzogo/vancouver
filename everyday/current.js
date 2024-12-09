18b32a059eb555d90054559c37395fd325d4bdea
dezzogo <122938506+dezzogo@users.noreply.github.com>
2024-12-09 09:05:45 -0300

    removing and editing some old files

--- everyday/current.js
+++ everyday/current.js
@@ -1,20 +1,18 @@
 
-function a(){
-  return document.querySelector("#wpcf7-f209-p6-o1 > form > p:nth-child(2) > label:nth-child(5) > span > input").value
-  }
 
-
-
-
-var emailEC = //copiar apenas abaixo
 function () {
-    var regex = /\S+@\S+\.\S+/;
-    var email = document.querySelector('[type="email"]');
-    if(!email) return;
-    if(!regex.test(email.value)) return;
-    return email.value;
+  var regex = /\S+@\S+\.\S+/;
+  var email = document.querySelector('[name="email"]');
+  if(!email) return;
+  if(!regex.test(email.value)) return;
+  return email.value;
 }
 
-
+function () {
+  var g_getPhone = document.querySelector('#rd-phone_field-m0r0s40l').value.replace(/\D/g, '')
+  if(g_getPhone.length >= 11 && g_getPhone.length <= 15) {
+     return "+" + g_getPhone
+  }
+}
 
 
