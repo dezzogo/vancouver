889a17cc429dc048fc26c9e10bd04ae11ec8c76d
dezzogo <122938506+dezzogo@users.noreply.github.com>
2024-12-18 14:12:09 -0300

    new way to get ec

---
+++ everyday/current_ec.js
@@ -0,0 +1,56 @@
+//EMAIL
+var email = //no needed, only to avoid erros
+
+function() {
+    var regex = /\S+@\S+\.\S+/;
+    var email;
+  
+    document.querySelectorAll('SELETOR_AQUI').forEach(function(e) {
+      if(e.value) {
+         email = e.value
+      }
+    });
+    if(!email) return;
+    if(!regex.test(email)) return;
+    return email.toLowerCase();
+  }
+
+  /* ---------- x ---------- */
+
+//PHONE NUMBER
+var phoneNumber = //no needed, only to avoid erros
+
+function() {
+    var g_countryCode = '55';
+    var g_getPhone;
+   
+    document.querySelectorAll('SELETOR_AQUI').forEach(function(e) {
+      if(e.value) {
+         g_getPhone = e.value
+      }
+    });
+    g_getPhone = g_countryCode + g_getPhone;
+    g_getPhone = g_getPhone.replace(/\D/g, '');
+    if(g_getPhone.length >= 11 && g_getPhone.length <= 15) {
+      return "+" + g_getPhone
+    }
+  }
+
+
+//PHONE NUMBER WITH +55 (TO TEST)
+var phone55 = 
+function () {
+  var g_getPhone;
+  var g_countryCode = document.querySelector('SELETOR_AQUI').value;
+
+  var g_getPhone = document.querySelectorAll('SELETOR AQUI').forEach(function(e) {
+    if(e.value) {
+       g_getPhone = e.value
+    }
+  });
+  g_getPhone = g_countryCode + g_getPhone;
+  g_getPhone = g_getPhone.replace(/\D/g, '');
+  if(g_getPhone.length >= 9 && g_getPhone.length <= 13) {
+    return "+" + g_getPhone
+  }
+}
--- everyday/sharing.js
+++ everyday/sharing.js
@@ -1,13 +1,29 @@
+document
+  .querySelector('[type="submit"]')
+  .addEventListener("click", function () {
+    if (
+      window.location.href.includes("c_campaign=614") &&
+      !document.body.innerText.includes("Required")
+    ) {
+      dataLayer.push({ event: "formulario_madeira" });
+      console.log("formulario_madeira");
+    }
+    if (
+      window.location.href.includes("c_campaign=613") &&
+      !document.body.innerText.includes("Required")
+    ) {
+      dataLayer.push({ event: "formulario_oeiras" });
+      console.log("formulario_oeiras");
+    }
+  });
 
-document.querySelector('[type="submit"]').addEventListener('click', function(){
-    if(window.location.href.includes('c_campaign=614') && !document.body.innerText.includes('Required')){
-        dataLayer.push({'event': 'formulario_madeira'});
-        console.log('formulario_madeira')
-    }
-    if(window.location.href.includes('c_campaign=613') && !document.body.innerText.includes('Required')){
-        dataLayer.push({'event': 'formulario_oeiras'});
-        console.log('formulario_oeiras')
-    }
-})
+if (document.body.innerText.includes("Your event has been scheduled successfully.") && window.location.href.includes("iss-madeira")) {
+  dataLayer.push({ event: "madeira_bookatour" });
+  console.log("madeira_bookatour");
+}
+if (document.body.innerText.includes("Your event has been scheduled successfully.") && window.location.href.includes("iss-taguspark")) {
+  dataLayer.push({ event: "oeiras_bookatour" });
+  console.log("oeiras_bookatour");
+}
 
 //once per page
