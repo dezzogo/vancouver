26bd1f99448fe5c5788b55119dea76b62aad0d5c
dezzogo <122938506+dezzogo@users.noreply.github.com>
2025-05-22 11:06:00 -0300

    portugalbywine

--- current_online/genterica.js
+++
@@ -1,9 +0,0 @@
-var meetingForm = document.querySelector('#schedule-meeting-button');
-if(meetingForm){
-    var email = document.querySelector('[type="email"]').value;
-    if(email){
-        dataLayer.push({
-            'event':'form_conferencia'
-        })
-    }
-}
--- current_online/portugalbywine.js
+++ current_online/portugalbywine.js
@@ -15,3 +15,24 @@ formTailorMade.addEventListener('submit', function () {
 })
 
 //</script>
+
+<script>
+var formContacto = document.querySelector("#cph1_modulocontactosformulario_lnk_enviar");
+var formTailorMade = document.querySelector("#cph1_moduloformularioamedida_lnk_enviar");
+
+if (formContacto) {
+    formContacto.addEventListener('click', function() {
+        dataLayer.push({
+            'event': 'form_contacto'
+        })
+    })
+}
+
+if (formTailorMade) {
+    formTailorMade.addEventListener('click', function() {
+        dataLayer.push({
+            'event': 'form_tailor_made'
+        })
+    })
+}
+</script>
